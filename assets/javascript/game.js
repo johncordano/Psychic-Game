781268b0e8a46f276157e43bc6720f01853e1e21
John Cordano <jecordano@yahoo.com>
2018-01-20 13:29:57 -0800

    Add functionality for guesses left, user wins, and user losses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,60 @@
 
-// Creates an array that lists out all of the computer options.
+// Creates an array that lists all of the computer choices.
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Creates an empty array that will list the user guesses.
 var guessesSofar = [];
+// Creates a variable for the guesses left from the user.
+var guessesLeft = 9;
+// Creates variable for the number of times the user guess is incorrect in a game session.
+var losses = 0;
+//Creates variable for the number of times the user guess is correct in a game session.
+var wins = 0;
 
 
 
- // This function is run when the user presses a key.
- document.onkeyup = function(event) {
 
- 	// Determines which key was pressed.
+// Randomly chooses a value from the computerChoices array. This value is the computer's guess.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+// This function is run when the user presses a key.
+document.onkeyup = function(event) {
+
+ 	// Determines the key that the user pressed.
  	var userGuess = event.key;
+ 	// Populates GuessesSofar array with the user key.
  	guessesSofar.push(userGuess);
+ 	// Updates the "Guesses so far" display.
  	document.getElementById("displayuserguesses").innerHTML = (guessesSofar);
- 	console.log(userGuess);
- 	console.log(guessesSofar);
- 	// console.log(guessesSofar.length);
+ 	// Determines the guesses left.
+ 	guessesLeft = (guessesLeft - 1);
+ 	// Updates the "Guesses Left" display.
+ 	document.getElementById("displayguessesleft").innerHTML = (guessesLeft);
+ 	//Compares the computer guess to the user guess to determine the number of wins and losses.
+	if ((userGuess) !== (computerGuess)) {
+		losses = (losses + 1);
+		}
+	else {
+		wins = (wins + 1);
+		}
+	//Updates the "Wins" display.
+	document.getElementById("displaywins").innerHTML = (wins);
+	//Updates the "Losses" display.
+ 	document.getElementById("displaylosses").innerHTML = (losses);
 
-	// Randomly chooses a choice from the options array. This choice is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log(computerGuess);
- };
+ 	if ((guessesLeft) < 1) {
+ 		wins = 0;
+ 		losses = 0;
+ 		guessesLeft = 0;
+	}
+
+ 	console.log(userGuess);
+ 	console.log(computerGuess);
+ 	console.log(guessesSofar);
+ 	console.log(guessesLeft);
+ 	console.log(losses);
+ 	console.log(wins);
+ 	
+};
+
+
