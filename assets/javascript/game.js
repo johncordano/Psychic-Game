ce6330b6bd336b294678f663c16ec679b9d8c925
John Cordano <jecordano@yahoo.com>
2018-01-20 21:18:51 -0800

    Set up function for html page loading, and adjusted wins and losses logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,27 @@ var wins = 0;
 
 
 
+
+
+//Loads the variables from the js to the html.
+function loadvar () {
+	var guessesLeft = 9;
+	document.getElementById("displayguessesleft").innerHTML = (guessesLeft);
+	var losses = 0;
+	document.getElementById("displaylosses").innerHTML = (losses);
+	var wins = 0;
+	document.getElementById("displaywins").innerHTML = (wins);
+	var guessesSofar = [];
+	document.getElementById("displayuserguesses").innerHTML = (guessesSofar);
+}
+
+
+
+
+
+
+
+
 // Randomly chooses a value from the computerChoices array. This value is the computer's guess.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
@@ -26,27 +47,42 @@ document.onkeyup = function(event) {
  	guessesSofar.push(userGuess);
  	// Updates the "Guesses so far" display.
  	document.getElementById("displayuserguesses").innerHTML = (guessesSofar);
- 	// Determines the guesses left.
+ 	// Resets the field values when a game session is complete.
+ 	// if ((guessesLeft) < 0) {
+ 	// 	(guessesLeft) = 9;
+ 	// 	(guessesSofar.length) = 0;
+ 	// }
+ 	// Determines the number of guesses left.
  	guessesLeft = (guessesLeft - 1);
  	// Updates the "Guesses Left" display.
  	document.getElementById("displayguessesleft").innerHTML = (guessesLeft);
- 	//Compares the computer guess to the user guess to determine the number of wins and losses.
-	if ((userGuess) !== (computerGuess)) {
-		losses = (losses + 1);
-		}
-	else {
+ 	//Resets the field values when a game session is complete.
+ 	// if ((guessesLeft) < 0) {
+ 	// (guessesLeft) = 9;
+ 	// (guessesSofar.length) = 0;
+ 	// }
+	// Determines the number of session wins.
+	if ((userGuess) == (computerGuess)) {
 		wins = (wins + 1);
+		//var sessiondone = true;		
 		}
-	//Updates the "Wins" display.
+	// //Updates the "Wins" display.
 	document.getElementById("displaywins").innerHTML = (wins);
+	// Determines the number of session losses.
+	if ((guessesLeft) == 0) {
+		losses = (losses +1);
+		//var sessiondone = true;
+	}
 	//Updates the "Losses" display.
  	document.getElementById("displaylosses").innerHTML = (losses);
+ 	//Resets the field values when the game session is complete.
+ 	// if sessiondone = true {
+ 	// (guessesLeft) = 9;
+ 	// (guessesSofar.length) = 0;
+ 	// }
 
- 	if ((guessesLeft) < 1) {
- 		wins = 0;
- 		losses = 0;
- 		guessesLeft = 0;
-	}
+
+ 	
 
  	console.log(userGuess);
  	console.log(computerGuess);
@@ -56,5 +92,16 @@ document.onkeyup = function(event) {
  	console.log(wins);
  	
 };
+
+//Resets field values when a game session is complete. This doesn't work.
+// if ((guessesLeft) < 1) {
+// 		(guessesLeft) = 9;
+// 		(guessesSofar.length) = 0;
+// }
+// else {
+// };
+
+
+
 
 
